6acfb6cfe4ef7ba4a9decd34260da7ffac2f280d
becky sama <samarebecca616@gmail.com>
2021-10-17 08:14:37 +0100

    Ajoutez des fichiers projet.

---
+++ MyLibsNet.Serialisation/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace MyLibs.Serialisation
+{
+    public class Serializer
+    {
+        public void Serialize<T>(T data, string parth)
+        {
+            using (StreamWriter file = new StreamWriter(parth, true))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(file.BaseStream, data);
+            }
+        }
+        public T Deserialize<T>(string parth)
+        {
+            using (StreamWriter file = new StreamWriter(parth, true))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                return (T)bf.Deserialize(file.BaseStream);
+            }
+        }
+    }
+}
